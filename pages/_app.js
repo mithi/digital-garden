acdcc3f8aa28a5e729bab5832cd4d0a4b857776c
Mithi <mithi.sevilla@gmail.com>
2020-11-12 17:20:12 +0800

    🌷 Apply syntax highlighting and other style changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import Layout from "../components/Layout"
+import "../styles/reset.css"
+import "../styles/prism-theme.css"
+import "../styles/prism-theme-override.css"
 import "../styles/globals.css"
+import Layout from "../components/Layout"
 
 const app = ({ Component, pageProps }) => (
     <Layout>
