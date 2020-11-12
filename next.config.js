77a8413dccbf13161967ac1450a163fac83a630a
Mithi <mithi.sevilla@gmail.com>
2020-11-12 17:18:45 +0800

    💕💕⭐⭐Add plugin for syntax highlighting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/
-  })
+const mdxGenerator = require("@next/mdx")
+
+const mdxOptions = {
+    options: {
+        rehypePlugins: [
+            require("@mapbox/rehype-prism"), // Syntax highlighting
+        ],
+    },
+}
+const withMDX = mdxGenerator(mdxOptions)
 
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
+    pageExtensions: ["js", "jsx", "mdx", "md", "ts", "tsx"],
 })
