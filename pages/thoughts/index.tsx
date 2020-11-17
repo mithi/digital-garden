bfbcf3fe5cd788cb6801ca819d4c52f320f3ec71
Mithi <mithi.sevilla@gmail.com>
2020-11-18 03:06:10 +0800

    💕 Add index per category

---
+++ pages/thoughts/index.tsx
@@ -0,0 +1,17 @@
+import { getAllNotesData } from "../../utils/utils"
+import NotePreview from "../../components/NotePreview"
+
+export async function getStaticProps() {
+    const notes = getAllNotesData().filter(data => data.id.match(/\/thoughts\//g))
+    return { props: { notes } }
+}
+
+const Home = ({ notes }) => (
+    <>
+        {notes.map(note => (
+            <NotePreview {...{ note, highlightTag: null }} key={note.id} />
+        ))}
+    </>
+)
+
+export default Home
